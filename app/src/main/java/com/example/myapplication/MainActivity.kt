26040e6eadf8f8f0ad7580666b5000deaa52f034
PavithraRanjana <dranjana56@gmail.com>
2024-06-12 14:16:33 +0530

    added 2 buttons

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,4 +18,11 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+
+    fun setViewEnable(view: View) : Unit {
+        view.isEnabled = false
+    }
+
+
 }
+
